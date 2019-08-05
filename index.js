94cf1cf169367d3baffa2cbee688511a7c963fb0
Geno M <giantpowpow2@gmail.com>
2019-08-05 00:52:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,4 +19,4 @@ function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
   return Object.assign(obj, {[key]: value});
 };
 
-console.log(destructivelyUpdateObjectWithKeyAndValue(recipes, Spice, 10));
+console.log(destructivelyUpdateObjectWithKeyAndValue(recipes, 'Spice', 10));
