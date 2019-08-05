41e00b9ff790b98c19f5e324a15915e6891b630f
Geno M <giantpowpow2@gmail.com>
2019-08-05 00:29:43 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,10 @@
+const recipes = {
+  'Bacon': '3 strips',
+  'Eggs': 3,
+  'Flour': '3 cups',
+  'Pasta': ['Spaghetti', 'Fetuccini'],
+  'Cooking Oil': '1 cup',
+  'Mushroom Soup': '1 can'
+};
+
+console.log(recipes);
